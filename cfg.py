f24eb75cbe603e1d8637d9420bce9a4debbed65e
Arief Ramadhan <arief25ramadhan@yahoo.com>
2020-04-06 23:03:55 +0700

    Delete cfg.py file

--- cfg.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    def __init__(self, mode='hcd'):
-        self.mode = mode
-        self.model_path = os.path.join('models',mode + '.model')
-        self.p_path = os.path.join('pickles', mode + '.p')
-
